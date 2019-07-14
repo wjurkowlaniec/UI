b26c2f954f5d400b7e013538459bc8adee5479b1
B desire <byadiso@gmail.com>
2019-07-14 08:17:45 +0200

    heroku file file Procfile

--- build/server.js
+++ build/server.js
@@ -1,20 +1,49 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _routes = require('./routes/routes');
+
+var _routes2 = _interopRequireDefault(_routes);
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+_dotenv2.default.config();
+
 var app = (0, _express2.default)();
-// server.js
 
-
+app.use(_express2.default.urlencoded({ extended: false }));
 app.use(_express2.default.json());
 
+app.use(_routes2.default);
+
 app.get('/', function (req, res) {
-  return res.status(200).send({ 'message': 'Welcome to my property web page' });
+    res.status(200).json({
+        status: 200,
+        message: 'Welcome to Property Pro Lite you can search properties for sale or rent!'
+    });
 });
 
-app.listen(3000);
-console.log('app running on port ', 3000);
+app.use('*', function (req, res) {
+    res.status(400).json({
+        status: 400,
+        message: "Sorry this router doesn't exist !"
+    });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    return console.log('Welcome to MY Property Pro Lite Land Server!!....');
+});
+
+exports.default = app;
